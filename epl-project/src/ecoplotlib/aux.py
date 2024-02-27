680cc03651a0b2befe46415b9677485f602d90b1
Janko Petkovic <j.petkovic91@gmail.com>
2024-02-27 21:52:22 +0100

    restructuring

--- epl-project/src/ecoplotlib/aux.py
+++
@@ -1,4 +0,0 @@
-def set_all(ax, **kwargs):
-    ax.set_title(kwargs.get('title'))
-    ax.set_xlabel(kwargs.get('xlabel'))
-    ax.set_ylabel(kwargs.get('ylabel'))
--- epl-project/src/ecoplotlib/plot.py
+++
@@ -1,6 +0,0 @@
-from .aux import *
-
-def plot(ax, X,Y, **kwargs):
-    out = ax.plot(X,Y, **kwargs)
-    set_all(ax, **kwargs)
-    return out
